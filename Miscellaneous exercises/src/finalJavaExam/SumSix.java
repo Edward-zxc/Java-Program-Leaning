4df26a99da639e83478ac92740052cc20a295009
Edward xc <3313338649@qq.com>
2024-01-07 18:53:45 +0800

    Update SumSix.java

--- Miscellaneous exercises/src/finalJavaExam/SumSix.java
+++ Miscellaneous exercises/src/finalJavaExam/SumSix.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
  * @author 33133
  * @see 7. 利用循环控制和键盘输入，获取用户输入的6个整数，输出该6个数的和。
  */
-public class SumSIx {
+public class SumSix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input six numbers");
         int sum = 0;
-        for (int i = 1; i <= 6; i++) {
-            System.out.println("请输入第"+i+"个整数：");
+        for (int i = 0; i < 6; i++) {
             int inputNumber = scanner.nextInt();
             sum += inputNumber;
         }
-        System.out.println("六个数和为："+sum);
+        System.out.println("The sum of the six numbers is："+sum);
+        scanner.close();
     }
 }
